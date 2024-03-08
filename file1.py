2718bcb9e0386c6c08e66d3fe46637b58050df63
ChavhanPallu <156051939+ChavhanPallu@users.noreply.github.com>
2024-03-08 19:09:12 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,6 +1,17 @@
-print("Hello World!")
-a=["priya","nita","pallavi","rashika"]
-i=0
-for i in a:
-        print(i)
-  
+n = 5
+
+# Upper half of the diamond
+for i in range(n):
+    for j in range(n - 1, i, -1):
+        print(" ", end="")
+    for k in range(i + 1):
+        print("* ", end="")
+    print()
+
+# Lower half of the diamond
+for i in range(1, n):
+    for j in range(i):
+        print(" ", end="")
+    for k in range(n - 1, i - 1, -1):
+        print("* ", end="")
+    print()
